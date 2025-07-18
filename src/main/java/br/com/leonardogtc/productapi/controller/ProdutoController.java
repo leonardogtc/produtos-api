13a5369a16a700285894a630cc77211bfb949912
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-07-18 01:32:34 -0300

    Persistindo os dados do produto

--- src/main/java/br/com/leonardogtc/productapi/controller/ProdutoController.java
+++ src/main/java/br/com/leonardogtc/productapi/controller/ProdutoController.java
@@ -1,18 +1,31 @@
 package br.com.leonardogtc.productapi.controller;
 
 import br.com.leonardogtc.productapi.model.Produto;
+import br.com.leonardogtc.productapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoController {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto salvo: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id); // Gera um ID único para o produto
+        // O Spring Data JPA cuida de salvar o produto no banco de dados
+        produtoRepository.save(produto);
         return produto;
     }
 }
---
+++ src/main/java/br/com/leonardogtc/productapi/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.leonardogtc.productapi.repository;
+
+import br.com.leonardogtc.productapi.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, String> {
+    // Este repositório herda métodos CRUD do JpaRepository
+    // Não é necessário implementar nada aqui, o Spring Data JPA faz isso automaticamente
+}
